cd5eb34a0a54daf1038b6dc9b540a6db0486da18
novakvova <novakvova@gmail>
2022-12-30 20:29:43 +0200

    Add RestApi Server

---
+++ WebServerAPI/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebServerAPI.Models;
+
+namespace WebServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController() { }
+
+
+        [HttpGet("list")]
+        public ActionResult Index()
+        {
+            List<RollItemViewModel> model = new List<RollItemViewModel>
+            {
+                new RollItemViewModel
+                {
+                    Id= 1,
+                    Name= "Спайсі рол з окунем і лососем",
+                    Grams= "210г",
+                    Description=new List<string> {
+                                    "Рол з окунем",
+                                    "лососем, листом салата",
+                                    "японським омлетом",
+                                    "спайсі соусом і ікрою Тобіко"
+                                },
+                    Img= "/menu/spajsi-roll-s-okunem-i-lososem.jpg",
+                    Discount= false,
+                    Price= 99,
+                    DiscountPrice= 70
+                }
+            };
+            return Ok(model);
+        }
+    }
+}
---
+++ WebServerAPI/Models/RollItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WebServerAPI.Models
+{
+    public class RollItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Grams { get; set; }
+        public List<string> Description { get; set; }
+        public string Img { get; set; }
+        public bool Discount { get; set; }
+        public int Price { get; set; }
+        public int DiscountPrice { get; set; }
+    }
+}
